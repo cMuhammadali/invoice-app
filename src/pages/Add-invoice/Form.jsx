5c78468a428270d82233c41b2b9735ab11e63bed
MuhammadAl1Dev <qahhorovmuhammadali011@gmail.com>
2023-12-13 16:04:51 +0500

    Form validationSchema

--- src/pages/Add-invoice/Form.jsx
+++ src/pages/Add-invoice/Form.jsx
@@ -11,14 +11,19 @@ import "./Form.css";
 
 export default function FormAdd() {
   const validationSchemaAddInvoice = Yup.object().shape({
-    clientName: Yup.string().required("Can't be empty"),
+    clientName: Yup.string()
+      .min(3, "Name must contain 3 characters")
+      .max(50, "Name must be at most 50 characters")
+      .required("Can't be empty"),
     clientDueDate: Yup.date().required("Can't be empty"),
     clientEmail: Yup.string()
       .email("Invalid email address")
       .required("Can't be empty"),
     clientPaymentTerms: Yup.string().required("Can't be empty"),
     clientDesc: Yup.string().required("Can't be empty"),
-    price: Yup.number().required("Can't be empty"),
+    price: Yup.number()
+      .min(100, "Must be at least 100").max(1000, "Must be less than 1000")
+      .required("Can't be empty"),
   });
 
   const navigate = useNavigate();
