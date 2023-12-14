714132ec0fa360dd1931344ced31b059332dcd6d
MuhammadAl1Dev <qahhorovmuhammadali011@gmail.com>
2023-12-14 15:12:14 +0500

    sidebar and rightbar jsx to js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as InputComponent } from './InputComponent/Input.jsx'
 export { default as ModalDelete } from "./Modal/Modal-delete.jsx";
-export { default as Rightbar } from "./Rightbar/Rightbar.jsx";
-export { default as Sidebar } from './Sidebar/Sidebar.jsx';
+export { default as Rightbar } from "./rightbar/rightbar";
 export { default as Navbar } from "./Navbar/Navbar.jsx";
+export { default as Sidebar } from './sidebar/sidebar';
 export { default as Main } from "./Main/Main.jsx";
 export { default as List } from "./List/List.jsx";
